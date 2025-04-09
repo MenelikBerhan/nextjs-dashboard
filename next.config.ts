a7805e205d87da31975e95649f692be068ba0a55
MenelikBerhan <menelikberhan@gmail.com>
2025-04-09 07:55:56 +0300

    Finish chapter 10

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // log cached or uncached routes for 'fetch' requests
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+
+  // // for partial prerendering
+  // experimental: {
+  //   ppr: 'incremental'
+  // }
 };
 
 export default nextConfig;
